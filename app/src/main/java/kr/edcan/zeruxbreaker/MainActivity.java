c82ec64a45344ba0f3f2ab2029a1273c6766c71e
KOTOHANA <fluorine2015@gmail.com>
2015-09-10 19:59:26 +0900

    Make Default Activities

--- app/src/main/java/kr/edcan/zeruxbreaker/MainActivity.java
+++ app/src/main/java/kr/edcan/zeruxbreaker/MainActivity.java
@@ -1,5 +1,6 @@
 package kr.edcan.zeruxbreaker;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.ActionBar;
@@ -8,9 +9,11 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.RelativeLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    RelativeLayout my_gesture, download, make_gesture;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,7 +22,30 @@ public class MainActivity extends AppCompatActivity {
         actionbar.setDisplayShowCustomEnabled(true);
         actionbar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#1E87E4")));
         actionbar.setCustomView(R.layout.actionbar_layout);
-
+        setDefault();
     }
 
+    private void setDefault() {
+        my_gesture = (RelativeLayout) findViewById(R.id.main_my_gesture);
+        download = (RelativeLayout) findViewById(R.id.main_download);
+        make_gesture = (RelativeLayout) findViewById(R.id.main_make_gesture);
+        my_gesture.setOnClickListener(this);
+        download.setOnClickListener(this);
+        make_gesture.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.main_my_gesture:
+                startActivity(new Intent(getApplicationContext(), MyGesture.class));
+                break;
+            case R.id.main_download:
+                startActivity(new Intent(getApplicationContext(), DownloadGesture.class));
+                break;
+            case R.id.main_make_gesture:
+                startActivity(new Intent(getApplicationContext(), MakeGesture.class));
+                break;
+        }
+    }
 }
